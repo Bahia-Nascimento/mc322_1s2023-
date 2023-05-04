ad03c1e21c8102c31e699c728d226a4531fc9b1c
Gabriel de Carvalho Silva Nascimento <127264380+Bahia-Nascimento@users.noreply.github.com>
2023-05-04 15:45:35 -0300

    Main.java renomeada para APPMAIN.java

--- lab4/src/AppMain.java
+++ lab4/src/AppMain.java
@@ -2,7 +2,7 @@ import java.time.LocalDate;
 import java.util.Scanner;
 import java.util.ArrayList;
 
-public class Main {
+public class AppMain {
     public static void main(String[] args) {
         boolean flag = true;
         ArrayList<Seguradora> listaSeguradoras = new ArrayList<Seguradora>();
